5fbe3d0229d9d96e4abd463efb709fc6fffe6c2d
Priyankar Kumar <priyankar.kumar98@gmail.com>
2017-01-06 20:44:28 +0530

    https

--- insert.js
+++ insert.js
@@ -3,7 +3,7 @@ $(document).ready(function()
 $.ajax({
   jsonp: "jsonp",
   dataType: "jsonp",
-  url: "http://api.forismatic.com/api/1.0/",
+  url: "https://api.forismatic.com/api/1.0/",
   contentType: "application/jsonp",
   data: {
     lang: "en",
